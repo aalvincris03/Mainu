c291d64f4f919142f65bc8fd8ba2f87f82783a03
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-03 06:49:28 +0800

    Update app.py

--- main/app.py
+++ main/app.py
@@ -159,14 +159,14 @@ def edit_debt(debt_id):
     debt.amount = float(request.form["amount"])
     debt.reason = request.form["reason"]
     debt.status = "status" in request.form
-    log_history(f"Edited debt for {debt.name.name}")
+    log_history(f"Edited {debt.name.name} {debt.amount.amount} = {debt.status.status}")
     db.session.commit()
     return "", 204
 
 @app.route("/delete/<int:debt_id>", methods=["POST"])
 def delete_debt(debt_id):
     debt = Debt.query.get_or_404(debt_id)
-    log_history(f"Deleted debt for {debt.name.name}")
+    log_history(f"Edited {debt.name.name} {debt.amount.amount} = {debt.status.status}")
     db.session.delete(debt)
     db.session.commit()
     return "", 204
