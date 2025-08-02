af51ab6387b06c3682ca3df850ebae8dc97f4a6c
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-03 02:13:01 +0800

    Update app.py

--- PWA/app.py
+++ PWA/app.py
@@ -14,8 +14,8 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.getenv('MAIL_USERNAME')
-app.config['MAIL_PASSWORD'] = os.getenv('MAIL_PASSWORD')
+app.config['MAIL_USERNAME'] = os.getenv('aalvincris@gmail.com')
+app.config['MAIL_PASSWORD'] = os.getenv('qwewer147')
 app.config['MAIL_DEFAULT_SENDER'] = app.config['MAIL_USERNAME']
 
 db = SQLAlchemy(app)
