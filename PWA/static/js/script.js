62258d0eedc7e4f8982925f484ff4b1960223744
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-03 01:29:46 +0800

    Add files via upload

--- PWA/static/js/script.js
+++ PWA/static/js/script.js
@@ -1,1 +1,7 @@
-
+document.getElementById('themeToggle').addEventListener('click', function () {
+  const html = document.documentElement;
+  const current = html.getAttribute('data-bs-theme');
+  const newTheme = current === 'dark' ? 'light' : 'dark';
+  html.setAttribute('data-bs-theme', newTheme);
+  document.getElementById('themeIcon').textContent = newTheme === 'dark' ? '🌙' : '☀️';
+});
