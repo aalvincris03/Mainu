e39a9fa53556f32a388b5dcda3a14be2dbe80c6a
eymaster <61962873+eymaster@users.noreply.github.com>
2025-08-03 03:11:24 +0800

    Update app.py

--- Split/app.py
+++ Split/app.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from models import db, Debt, History
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///utang.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 
